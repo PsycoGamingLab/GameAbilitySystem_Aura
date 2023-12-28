2e9f114c6040ff1a04cd264694e3390865efc827
PsycoGamingLab <psycogaminglab@gmail.com>
2023-12-28 11:52:17 +0100

    Added Player State

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,10 +5,76 @@
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	CursorTrace();
+}
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility,false,CursorHit);
+	if(!CursorHit.bBlockingHit)return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+	
+	/**
+	 ** Line trace from cursor. There are serveral scenarios:
+	 * A. Last Actor is null && ThisActor is null
+	 *		- Do nothing
+	 * B. Last Actor is null && ThisActor is valid
+	 *		- Highlight ThisActor
+     * C. Last Actor is valid && ThisActor is null
+     *		- UnHighlight LastActor
+     * D. Both actors are valid, but LastActor != ThisActor
+     *		- UnHighlight LastActor, and Highlight ThisActor
+     * E. Both Actors are valid, and are the same actor
+     *		- Do nothing
+	 **/
+
+	if(LastActor == nullptr)
+	{
+		if(ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A - both are null, do nothing
+		}
+	}
+	else // LastActor Is valid
+	{
+		if(ThisActor==nullptr)
+		{
+			//Case C
+			LastActor->UnHighlightActor();
+		}
+		else // both actors are valid
+		{
+			if(LastActor != ThisActor)
+			{
+				//Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				//Case E - Do nothing				
+			}
+		}
+	}
+	
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -53,3 +119,4 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
 	}
 }
+
