e6289dafbbf8afedbbf78aac5455d0763404d9db
PsycoGamingLab <psycogaminglab@gmail.com>
2024-01-03 18:57:38 +0100

    Added progress bar logic

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -3,3 +3,15 @@
 
 #include "UI/WidgetController/AuraWidgetController.h"
 
+void UAuraWidgetController::SetWidgetControllersParams(const FWidgetControllerParams& WCParams)
+{
+	PlayerController = WCParams.PlayerController;
+	PlayerState = WCParams.PlayerState;
+	AbilitySystemComponent = WCParams.AbilitySystemComponent;
+	AttributeSet = WCParams.AttributeSet;
+}
+
+void UAuraWidgetController::BroadcastInitialValues()
+{
+	
+}
