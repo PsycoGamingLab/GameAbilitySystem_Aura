7565df21b9fc0cc5bca256f516b8375d10bf810e
matte <mbologna@metalcode.it>
2025-02-10 17:55:37 +0100

    Fix player controller IEnemyInterface issue

---
+++ Source/Aura/Private/AbilitySystem/ModMagnitudeCalculation/MMC_MaxHealth.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/ModMagnitudeCalculation/MMC_MaxHealth.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource =EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather tags form source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Vigor =0.0f;
+	GetCapturedAttributeMagnitude(VigorDef,Spec,EvaluationParameters,Vigor);
+
+	return 0.f;
+}
